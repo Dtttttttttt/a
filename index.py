508871cc906c76ce817f6fa14f31b0b0c6ba7366
Dtttttttttt <dt0918274@gmail.com>
2023-11-28 11:52:54 +0800

    v2

--- index.py
+++ index.py
@@ -25,7 +25,7 @@ def index():
     homepage += "<a href=/account>帳號密碼</a><br>"
     homepage += "<a href=/addbooks>圖書精選</a><br>"
     homepage += "<br><a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
-    homepage += "<br><a href=/searchQ>讀取開眼電影即將上映影片</a><br>"
+     
     return homepage
 
 
@@ -159,22 +159,7 @@ def search():
             info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"
 
     return info    
-@app.route("/searchQ", methods=["POST","GET"])
 
-def searchQ():
-    if request.method == "POST":
-        MovieTitle = request.form["MovieTitle"]
-        info = ""
-        db = firestore.client()
-        collection_ref = db.collection("電影")
-        docs = collection_ref.order_by("showDate").get()
-        for doc in docs:
-            if MovieTitle in doc.to_dict()["title"]:
-                info += "片名：" + doc.to_dict()["title"] + "<br>"
-                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
-                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>"
-                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"
-    return info
 
 else:
     return render_template("input.html")
