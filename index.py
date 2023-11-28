c783e27c9f3c88ed5635a81668912fba178ff44b
Dtttttttttt <dt0918274@gmail.com>
2023-11-28 12:01:02 +0800

    v2

--- index.py
+++ index.py
@@ -25,7 +25,8 @@ def index():
     homepage += "<a href=/account>帳號密碼</a><br>"
     homepage += "<a href=/addbooks>圖書精選</a><br>"
     homepage += "<br><a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
-     
+    homepage += "<br><a href=/searchQ>查詢電影</a><br>"
+ 
     return homepage
 
 
@@ -160,8 +161,40 @@ def search():
 
     return info    
 
+@app.route("/searchQ", methods=["POST","GET"])
 
-else:
-    return render_template("input.html")
+def searchQ():
+
+    if request.method == "POST":
+
+        MovieTitle = request.form["MovieTitle"]
+
+        info = ""
+
+        db = firestore.client()
+
+        collection_ref = db.collection("電影")
+
+        docs = collection_ref.order_by("showDate").get()
+
+        for doc in docs:
+
+            if MovieTitle in doc.to_dict()["title"]:
+
+                info += "片名：" + doc.to_dict()["title"] + "<br>"
+
+                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+
+                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>"
+
+                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"
+
+        return info
+
+    else:
+
+        return render_template("input.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
